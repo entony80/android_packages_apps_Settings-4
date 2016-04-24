trader418/android_packages_apps_Settings

[2/2] Settings: Add ability to permanently hide apps from recents
Layout and java code is based off
https://github.com/zst123/MultiWindowSidebar with parts of
c080b620d48a967ade9780c90b13104eb5b88361

Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

Change-Id: I797e6de8bf22deee30183b027f3c5076e3049590

src/com/android/settings/temasek/HAFRAppChooserDialog.java
@@ -1,0 +1,80 @@
+package com.android.settings.temasek;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.Filter;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.AdapterView.OnItemClickListener;
+
+import com.android.settings.R;
+
+public abstract class HAFRAppChooserDialog extends Dialog {
+
+    final HAFRAppChooserAdapter dAdapter;
+    final ProgressBar dProgressBar;
+    final ListView dListView;
+    final EditText dSearch;
+    final ImageButton dButton;
+
+    private int mId;
+
+    public HAFRAppChooserDialog(Context context) {
+        super(context);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.dialog_app_chooser_list);
+
+        dListView = (ListView) findViewById(R.id.listView1);
+        dSearch = (EditText) findViewById(R.id.searchText);
+        dButton = (ImageButton) findViewById(R.id.searchButton);
+        dProgressBar = (ProgressBar) findViewById(R.id.progressBar1);
+
+        dAdapter = new HAFRAppChooserAdapter(context) {
+            @Override
+            public void onStartUpdate() {
+                dProgressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onFinishUpdate() {
+                dProgressBar.setVisibility(View.GONE);
+            }
+        };
+
+        dListView.setAdapter(dAdapter);
+        dListView.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> av, View v, int pos, long id) {
+                HAFRAppChooserAdapter.AppItem info = (HAFRAppChooserAdapter.AppItem) av
+                        .getItemAtPosition(pos);
+                onListViewItemClick(info, mId);
+                dismiss();
+            }
+        });
+
+        dButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dAdapter.getFilter().filter(dSearch.getText().toString(), new Filter.FilterListener() {
+                    public void onFilterComplete(int count) {
+                        dAdapter.update();
+                    }
+                });
+            }
+        });
+
+        dAdapter.update();
+    }
+
+    public void show(int id) {
+        mId = id;
+        show();
+    }
+
+    public abstract void onListViewItemClick(HAFRAppChooserAdapter.AppItem info, int id);
+}

src/com/android/settings/temasek/HAFRAppListAdapter.java
@@ -1,0 +1,152 @@
+package com.android.settings.temasek;
+
+
+import java.util.LinkedList;
+import java.util.Map;
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.settings.R;
+
+public abstract class HAFRAppListAdapter extends BaseAdapter {
+
+    final Context mContext;
+    final Handler mHandler;
+    final PackageManager mPackageManager;
+    final LayoutInflater mLayoutInflater;
+
+    protected LinkedList<PackageItem> mApps = new LinkedList<PackageItem>();
+
+    public HAFRAppListAdapter(Context context, Map<String, Integer> list) {
+        mContext = context;
+        mHandler = new Handler();
+        mPackageManager = context.getPackageManager();
+        mLayoutInflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        update(list);
+    }
+
+    public void update(final Map<String, Integer> app_array) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (mApps) {
+                    final PackageItem[] array = new PackageItem[app_array.size() * 2];
+                    for (String pkg_name : app_array.keySet()) {
+                        try {
+                            ApplicationInfo ai = mPackageManager.getApplicationInfo(pkg_name, 0);
+                            final PackageItem item = new PackageItem();
+                            item.title = ai.loadLabel(mPackageManager);
+                            item.icon = ai.loadIcon(mPackageManager);
+                            item.packageName = ai.packageName;
+                            array[app_array.get(pkg_name)] = item;
+                        } catch (Exception e) {
+                        }
+                    }
+
+                    final LinkedList<PackageItem> temp = new LinkedList<PackageItem>();
+                    for (int x = 0; x < array.length; x++) {
+                        if (array[x] != null) {
+                            temp.add(array[x]);
+                        }
+                    }
+                    mHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            mApps.clear();
+                            mApps = temp;
+                            notifyDataSetChanged();
+                        }
+                    });
+                }
+            }
+        }).start();
+    }
+
+    @Override
+    public int getCount() {
+        return mApps.size();
+    }
+
+    public LinkedList<PackageItem> getList() {
+        return mApps;
+    }
+
+    @Override
+    public PackageItem getItem(int position) {
+        return mApps.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        if (position < 0 || position >= mApps.size()) {
+            return -1;
+        }
+        return mApps.get(position).hashCode();
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView != null) {
+            holder = (ViewHolder) convertView.getTag();
+        } else {
+            convertView = mLayoutInflater.inflate(R.layout.view_package_list, parent, false);
+            holder = new ViewHolder();
+            holder.name = (TextView) convertView.findViewById(android.R.id.title);
+            holder.icon = (ImageView) convertView.findViewById(android.R.id.icon);
+            holder.pkg = (TextView) convertView.findViewById(android.R.id.message);
+            holder.remove = (ImageButton) convertView.findViewById(R.id.removeButton);
+            convertView.setTag(holder);
+        }
+        final PackageItem appInfo = getItem(position);
+
+        holder.name.setText(appInfo.title);
+        holder.pkg.setText(appInfo.packageName);
+        holder.icon.setImageDrawable(appInfo.icon);
+        holder.remove.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onRemoveButtonPress(appInfo);
+            }
+        });
+        return convertView;
+    }
+
+    public abstract void onRemoveButtonPress(PackageItem app_info);
+
+    public class PackageItem implements Comparable<PackageItem> {
+        public CharSequence title;
+        public String packageName;
+        public Drawable icon;
+
+        @Override
+        public int compareTo(PackageItem another) {
+            return this.title.toString().compareTo(another.title.toString());
+        }
+    }
+
+    static class ViewHolder {
+        TextView name;
+        ImageView icon;
+        TextView pkg;
+        ImageButton remove;
+    }
+}

src/com/android/settings/temasek/RecentsPanelSettings.java
@@ -20,6 +20,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.ContentResolver;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.database.ContentObserver;
 import android.os.Bundle;
@@ -65,6 +66,7 @@ public class RecentsPanelSettings extends SettingsPreferenceFragment implements
     private static final String SHOW_CLEAR_ALL_RECENTS = "show_clear_all_recents";
     private static final String RECENTS_DISMISS_ALL = "recents_clear_all_dismiss_all";
     private static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
+    private static final String PREF_HIDDEN_RECENTS_APPS_START = "hide_app_from_recents";
 
     // Preferences
     private static final String USE_SLIM_RECENTS = "use_slim_recents";
@@ -85,6 +87,12 @@ public class RecentsPanelSettings extends SettingsPreferenceFragment implements
     private static final String RECENT_CARD_TEXT_COLOR =
             "recent_card_text_color";
 
+    // Package name of the hidden recetns apps activity
+    public static final String HIDDEN_RECENTS_PACKAGE_NAME = "com.android.settings";
+    // Intent for launching the hidden recents actvity
+    public static Intent INTENT_HIDDEN_RECENTS_SETTINGS = new Intent(Intent.ACTION_MAIN)
+            .setClassName(HIDDEN_RECENTS_PACKAGE_NAME,
+            HIDDEN_RECENTS_PACKAGE_NAME + ".temasek.HAFRAppListActivity");
 
     private SwitchPreference mRecentsSearchBar;
     private SwitchPreference mRecentsMemBar;
@@ -102,6 +110,7 @@ public class RecentsPanelSettings extends SettingsPreferenceFragment implements
     private ColorPickerPreference mRecentPanelBgColor;
     private ColorPickerPreference mRecentCardBgColor;
     private ColorPickerPreference mRecentCardTextColor;
+    private Preference mHiddenRecentsApps;
 
     private static final int MENU_RESET = Menu.FIRST;
     private static final int DEFAULT_BACKGROUND_COLOR = 0x00ffffff;
@@ -134,6 +143,8 @@ public class RecentsPanelSettings extends SettingsPreferenceFragment implements
         mUseSlimRecents.setChecked(Settings.System.getInt(resolver,
                 Settings.System.USE_SLIM_RECENTS, 0) == 1);
         mUseSlimRecents.setOnPreferenceChangeListener(this);
+
+        mHiddenRecentsApps = (Preference) prefSet.findPreference(PREF_HIDDEN_RECENTS_APPS_START);
 
         updatePreference();
     }
@@ -247,6 +258,16 @@ public class RecentsPanelSettings extends SettingsPreferenceFragment implements
             Settings.System.putInt(getContentResolver(),
                 Settings.System.RECENTS_MAX_APPS, value);
             return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mHiddenRecentsApps) {
+            getActivity().startActivity(INTENT_HIDDEN_RECENTS_SETTINGS);
+        } else {
+            return super.onPreferenceTreeClick(preferenceScreen, preference);
         }
         return false;
     }

entony80/android_packages_apps_Settings-4

Conflicts:
	AndroidManifest.xml

src/com/android/settings/cyanpop/HAFRAppChooserDialog.java
@@ -1,0 +1,80 @@
+package com.android.settings.cyanpop;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.Filter;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.AdapterView.OnItemClickListener;
+
+import com.android.settings.R;
+
+public abstract class HAFRAppChooserDialog extends Dialog {
+
+    final HAFRAppChooserAdapter dAdapter;
+    final ProgressBar dProgressBar;
+    final ListView dListView;
+    final EditText dSearch;
+    final ImageButton dButton;
+
+    private int mId;
+
+    public HAFRAppChooserDialog(Context context) {
+        super(context);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.dialog_app_chooser_list);
+
+        dListView = (ListView) findViewById(R.id.listView1);
+        dSearch = (EditText) findViewById(R.id.searchText);
+        dButton = (ImageButton) findViewById(R.id.searchButton);
+        dProgressBar = (ProgressBar) findViewById(R.id.progressBar1);
+
+        dAdapter = new HAFRAppChooserAdapter(context) {
+            @Override
+            public void onStartUpdate() {
+                dProgressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onFinishUpdate() {
+                dProgressBar.setVisibility(View.GONE);
+            }
+        };
+
+        dListView.setAdapter(dAdapter);
+        dListView.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> av, View v, int pos, long id) {
+                HAFRAppChooserAdapter.AppItem info = (HAFRAppChooserAdapter.AppItem) av
+                        .getItemAtPosition(pos);
+                onListViewItemClick(info, mId);
+                dismiss();
+            }
+        });
+
+        dButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dAdapter.getFilter().filter(dSearch.getText().toString(), new Filter.FilterListener() {
+                    public void onFilterComplete(int count) {
+                        dAdapter.update();
+                    }
+                });
+            }
+        });
+
+        dAdapter.update();
+    }
+
+    public void show(int id) {
+        mId = id;
+        show();
+    }
+
+    public abstract void onListViewItemClick(HAFRAppChooserAdapter.AppItem info, int id);
+}

src/com/android/settings/cyanpop/HAFRAppListAdapter.java
@@ -1,0 +1,152 @@
+package com.android.settings.cyanpop;
+
+
+import java.util.LinkedList;
+import java.util.Map;
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.settings.R;
+
+public abstract class HAFRAppListAdapter extends BaseAdapter {
+
+    final Context mContext;
+    final Handler mHandler;
+    final PackageManager mPackageManager;
+    final LayoutInflater mLayoutInflater;
+
+    protected LinkedList<PackageItem> mApps = new LinkedList<PackageItem>();
+
+    public HAFRAppListAdapter(Context context, Map<String, Integer> list) {
+        mContext = context;
+        mHandler = new Handler();
+        mPackageManager = context.getPackageManager();
+        mLayoutInflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        update(list);
+    }
+
+    public void update(final Map<String, Integer> app_array) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (mApps) {
+                    final PackageItem[] array = new PackageItem[app_array.size() * 2];
+                    for (String pkg_name : app_array.keySet()) {
+                        try {
+                            ApplicationInfo ai = mPackageManager.getApplicationInfo(pkg_name, 0);
+                            final PackageItem item = new PackageItem();
+                            item.title = ai.loadLabel(mPackageManager);
+                            item.icon = ai.loadIcon(mPackageManager);
+                            item.packageName = ai.packageName;
+                            array[app_array.get(pkg_name)] = item;
+                        } catch (Exception e) {
+                        }
+                    }
+
+                    final LinkedList<PackageItem> temp = new LinkedList<PackageItem>();
+                    for (int x = 0; x < array.length; x++) {
+                        if (array[x] != null) {
+                            temp.add(array[x]);
+                        }
+                    }
+                    mHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            mApps.clear();
+                            mApps = temp;
+                            notifyDataSetChanged();
+                        }
+                    });
+                }
+            }
+        }).start();
+    }
+
+    @Override
+    public int getCount() {
+        return mApps.size();
+    }
+
+    public LinkedList<PackageItem> getList() {
+        return mApps;
+    }
+
+    @Override
+    public PackageItem getItem(int position) {
+        return mApps.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        if (position < 0 || position >= mApps.size()) {
+            return -1;
+        }
+        return mApps.get(position).hashCode();
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView != null) {
+            holder = (ViewHolder) convertView.getTag();
+        } else {
+            convertView = mLayoutInflater.inflate(R.layout.view_package_list, parent, false);
+            holder = new ViewHolder();
+            holder.name = (TextView) convertView.findViewById(android.R.id.title);
+            holder.icon = (ImageView) convertView.findViewById(android.R.id.icon);
+            holder.pkg = (TextView) convertView.findViewById(android.R.id.message);
+            holder.remove = (ImageButton) convertView.findViewById(R.id.removeButton);
+            convertView.setTag(holder);
+        }
+        final PackageItem appInfo = getItem(position);
+
+        holder.name.setText(appInfo.title);
+        holder.pkg.setText(appInfo.packageName);
+        holder.icon.setImageDrawable(appInfo.icon);
+        holder.remove.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onRemoveButtonPress(appInfo);
+            }
+        });
+        return convertView;
+    }
+
+    public abstract void onRemoveButtonPress(PackageItem app_info);
+
+    public class PackageItem implements Comparable<PackageItem> {
+        public CharSequence title;
+        public String packageName;
+        public Drawable icon;
+
+        @Override
+        public int compareTo(PackageItem another) {
+            return this.title.toString().compareTo(another.title.toString());
+        }
+    }
+
+    static class ViewHolder {
+        TextView name;
+        ImageView icon;
+        TextView pkg;
+        ImageButton remove;
+    }
+}

src/com/android/settings/cyanpop/RecentsPanelSettings.java
@@ -20,6 +20,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.ContentResolver;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.database.ContentObserver;
 import android.os.Bundle;
@@ -65,6 +66,7 @@ public class RecentsPanelSettings extends SettingsPreferenceFragment implements
     private static final String SHOW_CLEAR_ALL_RECENTS = "show_clear_all_recents";
     private static final String RECENTS_DISMISS_ALL = "recents_clear_all_dismiss_all";
     private static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
+    private static final String PREF_HIDDEN_RECENTS_APPS_START = "hide_app_from_recents";
 
     // Preferences
     private static final String USE_SLIM_RECENTS = "use_slim_recents";
@@ -85,6 +87,12 @@ public class RecentsPanelSettings extends SettingsPreferenceFragment implements
     private static final String RECENT_CARD_TEXT_COLOR =
             "recent_card_text_color";
 
+    // Package name of the hidden recetns apps activity
+    public static final String HIDDEN_RECENTS_PACKAGE_NAME = "com.android.settings";
+    // Intent for launching the hidden recents actvity
+    public static Intent INTENT_HIDDEN_RECENTS_SETTINGS = new Intent(Intent.ACTION_MAIN)
+            .setClassName(HIDDEN_RECENTS_PACKAGE_NAME,
+            HIDDEN_RECENTS_PACKAGE_NAME + ".cyanpop.HAFRAppListActivity");
 
     private SwitchPreference mRecentsSearchBar;
     private SwitchPreference mRecentsMemBar;
@@ -102,6 +110,7 @@ public class RecentsPanelSettings extends SettingsPreferenceFragment implements
     private ColorPickerPreference mRecentPanelBgColor;
     private ColorPickerPreference mRecentCardBgColor;
     private ColorPickerPreference mRecentCardTextColor;
+    private Preference mHiddenRecentsApps;
 
     private static final int MENU_RESET = Menu.FIRST;
     private static final int DEFAULT_BACKGROUND_COLOR = 0x00ffffff;
@@ -134,6 +143,8 @@ public class RecentsPanelSettings extends SettingsPreferenceFragment implements
         mUseSlimRecents.setChecked(Settings.System.getInt(resolver,
                 Settings.System.USE_SLIM_RECENTS, 0) == 1);
         mUseSlimRecents.setOnPreferenceChangeListener(this);
+
+        mHiddenRecentsApps = (Preference) prefSet.findPreference(PREF_HIDDEN_RECENTS_APPS_START);
 
         updatePreference();
     }
@@ -247,6 +258,16 @@ public class RecentsPanelSettings extends SettingsPreferenceFragment implements
             Settings.System.putInt(getContentResolver(),
                 Settings.System.RECENTS_MAX_APPS, value);
             return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mHiddenRecentsApps) {
+            getActivity().startActivity(INTENT_HIDDEN_RECENTS_SETTINGS);
+        } else {
+            return super.onPreferenceTreeClick(preferenceScreen, preference);
         }
         return false;
     }